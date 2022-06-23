1597c0aefb2746d561756319891e04faf8cda776
Mridul Dey <51965740+mriduldey@users.noreply.github.com>
2022-06-24 01:53:52 +0530

    timer stop reset start timer stop reset start

---
+++ src/useEffect/Timer2.js
@@ -0,0 +1,31 @@
+import { useState, useRef } from "react";
+
+export default function Timer2() {
+  const [count, setCount] = useState(0);
+  const timerId = useRef(null);
+
+  const tick = () => {
+    setCount((count) => count + 1);
+  };
+
+  const start = () => {
+    timerId.current = setInterval(tick, 1000);
+  };
+
+  const stop = () => clearInterval(timerId.current);
+
+  const reset = () => {
+    stop();
+    setCount(0);
+  };
+
+  return (
+    <>
+      <h2> Timer With Function </h2>
+      <div>{count}s</div>
+      <button onClick={start}>Start</button>
+      <button onClick={stop}>Stop</button>
+      <button onClick={reset}>Reset</button>
+    </>
+  );
+}
